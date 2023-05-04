4f18f0d8bfb6727ea407b21f592a25c4f264e576
Pedro Henrique A. Silva <pedro.henrique.a.s66@gmail.com>
2023-05-04 18:41:35 -0300

    Requisito 5 concluido

--- src/functions.js
+++ src/functions.js
@@ -1,41 +1,41 @@
 // Requisito 1 - Crie a função verificaPalindromo
 
-function invertePalavra(palavra){
+function invertePalavra(palavra) {
   let palavraInvertida = '';
 
-  for (let index = 0; index < palavra.length; index += 1){
-    palavraInvertida += palavra[(palavra.length - index) -1]
+  for (let index = 0; index < palavra.length; index += 1) {
+    palavraInvertida += palavra[(palavra.length - index) - 1];
   }
 
-  return palavraInvertida
+  return palavraInvertida;
 }
 
-function verificaPalindromo(palavra){
+function verificaPalindromo(palavra) {
   let palavraInvertida = invertePalavra(palavra);
 
   if (palavraInvertida !== palavra) return false;
 
-  return true
+  return true;
 }
 
-let palavra = 'aboba';
+// let palavra = 'aboba';
 
-if (verificaPalindromo(palavra) === true){
-  console.log(palavra,'é um palindromo');
-}else{
-  console.log(palavra,'não é um palindromo');
-}
+// if (verificaPalindromo(palavra) === true){
+//   console.log(palavra,'é um palindromo');
+// }else{
+//   console.log(palavra,'não é um palindromo');
+// }
 
 // Requisito 2 - Crie a função indiceDoMaior
 
-let array = [20, 33, 60, 7, 10, 1];
+// let array = [20, 33, 60, 7, 10, 1];
 
-function indiceDoMaior(lista){
+function indiceDoMaior(lista) {
   let maiorValor = lista[0];
   let indiceMaiorValor = 0;
 
-  for (let index = 0; index < lista.length; index += 1){
-    if (lista[index] > maiorValor){
+  for (let index = 0; index < lista.length; index += 1) {
+    if (lista[index] > maiorValor) {
       maiorValor = lista[index];
       indiceMaiorValor = index;
     }
@@ -48,14 +48,14 @@ function indiceDoMaior(lista){
 
 // Requisito 3 - Crie a função indiceDoMenor
 
-array = [20, 33, 60, 7, 10, 10];
+// array = [20, 33, 60, 7, 10, 10];
 
-function indiceDoMenor(lista){
+function indiceDoMenor(lista) {
   let menorValor = lista[0];
   let indiceMenorValor = 0;
 
-  for (let index = 0; index < lista.length; index += 1){
-    if (lista[index] < menorValor){
+  for (let index = 0; index < lista.length; index += 1) {
+    if (lista[index] < menorValor) {
       menorValor = lista[index];
       indiceMenorValor = index;
     }
@@ -64,26 +64,48 @@ function indiceDoMenor(lista){
   return indiceMenorValor;
 }
 
-console.log(indiceDoMenor(array));
+// console.log(indiceDoMenor(array));
 
 // Requisito 4 - Crie a função maiorPalavra
 
-array = ['pedro', 'fernanda', 'mauricio', 'jose']
+// array = ['pedro', 'fernanda', 'mauricio', 'jose'];
 
-function maiorPalavra(lista){
+function maiorPalavra(lista) {
   let maior = '';
 
-  for (let item of lista){
-    if (item.length > maior.length){
+  for (let item of lista) {
+    if (item.length > maior.length) {
       maior = item;
     }
   }
 
-  return maior
+  return maior;
 }
 
 // Requisito 5 - Crie a função maisRepetido
 
+let array = [2, 3, 3, 5, 3, 8, 2, 3];
+
+function maisRepetido(lista) {
+  let maisFrequenteValor = 0;
+  let frequencia = 0;
+  for (let index = 0; index < lista.length; index += 1) {
+    let contador = 0;
+    for (let value of lista) {
+      if (lista[index] === value) {
+        contador += 1;
+      }
+    }
+
+    if (contador > frequencia) {
+      frequencia = contador;
+      maisFrequenteValor = lista[index];
+    }
+  }
+  return maisFrequenteValor;
+}
+
+console.log(maisRepetido(array));
 // Requisito 6 - Crie a função somatorio
 
 // Requisito 7 - Crie a função verificaFimPalavra
